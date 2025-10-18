32cfe166dc226bee85c3ebf00e0a5be90148cb96
wave745 <shazarno1@gmail.com>
2025-10-18 14:16:02 +0000

    Add customizable background and filter options to the trenches page
    Integrate TrenchesBackground and BackgroundCustomizer components into the trenches page, allowing users to upload and adjust background image filters.

--- app/trenches/page.tsx
+++ app/trenches/page.tsx
@@ -1,5 +1,12 @@
 import { TrenchesPage } from "@/components/trenches-page"
+import { TrenchesBackground } from "@/components/trenches-background"
+import { BackgroundCustomizer } from "@/components/background-customizer"
 
 export default function Trenches() {
-  return <TrenchesPage />
+  return (
+    <TrenchesBackground>
+      <TrenchesPage />
+      <BackgroundCustomizer />
+    </TrenchesBackground>
+  )
 }
