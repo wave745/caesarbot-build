59edb70657ea1b24dfdaa26df7e34d628b757a85
wave745 <shazarno1@gmail.com>
2025-10-20 14:54:43 +0000

    Remove custom background feature from the trenches page
    Removes the `TrenchesBackground` component and associated background customization logic from the trenches page, including `BackgroundCustomizer` and `BackgroundCustomizerInline` components and related local storage handling.

--- app/trenches/page.tsx
+++ app/trenches/page.tsx
@@ -1,10 +1,5 @@
 import { TrenchesPage } from "@/components/trenches-page"
-import { TrenchesBackground } from "@/components/trenches-background"
 
 export default function Trenches() {
-  return (
-    <TrenchesBackground>
-      <TrenchesPage />
-    </TrenchesBackground>
-  )
+  return <TrenchesPage />
 }
